576d44656ad08ab16fa306c56a1830b254988f1b
Lachlan Bird <lachlan.bird@99designs.com>
2021-03-18 16:30:09 +1100

    Expose `fargateService` in the `shared` module

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,10 +1,9 @@
-export {default as alb}  from "./alb";
-export {default as elb}  from "./elb";
-export {default as fargate}  from "./fargate";
-export {default as healthcheck}  from "./healthcheck";
-export {default as mute}  from "./mute";
-export {default as rds}  from "./rds";
-export {default as burstablerds} from "./burstablerds";
+export { default as alb } from "./alb";
+export { default as elb } from "./elb";
+export { default as fargate } from "./fargate";
+export { default as healthcheck } from "./healthcheck";
+export { default as mute } from "./mute";
+export { default as rds } from "./rds";
+export { default as burstablerds } from "./burstablerds";
+export { default as fargateService } from "./fargateService";
 export * from "./styles";
-
-
